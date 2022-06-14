bc167500759e832d6b7efc4f423b86d6cdee6300
AdriDanindra <adrian.18051@mhs.its.ac.id>
2022-06-14 20:49:10 +0700

    add contact

--- EAS_PBKK/routes/web.php
+++ EAS_PBKK/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\homeController;
 use App\Http\Controllers\mainController;
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\registerController;
+use App\Http\Controllers\contactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,4 +26,5 @@ Route::get('login',[loginController::class,'index'])->name('login');
 Route::get('main',[mainController::class,'index'])->name('home');
 Route::get('register',[registerController::class,'index'])->name('register');
 Route::get('admin',[adminController::class,'index'])->name('admin');
+Route::get('contact',[contactController::class,'index'])->name('contact');
 Route::post('submit', [loginController::class,'submit']);
