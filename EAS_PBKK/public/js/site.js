6baa4cb90b159a9715055543e09b476deaca6c69
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2022-06-18 19:21:24 +0700

    Update site.js

--- EAS_PBKK/public/js/site.js
+++ EAS_PBKK/public/js/site.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     $('.carousel').carousel({
       interval: 3000
     })
-  });  
+    // Data Picker Initialization
+    
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+        $('#datetimepicker2').datetimepicker({useCurrent: false});
+        $("#datetimepicker1").on("dp.change", function (e) {
+            $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker2").on("dp.change", function (e) {
+            $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+        });
+    });
+});  
